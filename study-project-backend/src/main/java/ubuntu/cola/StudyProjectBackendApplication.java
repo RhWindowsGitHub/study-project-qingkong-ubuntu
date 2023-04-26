724f79451a8ec2404646b70668094c6806c0791b
ColaUbuntu <cola0223Ubuntu@gmail.com>
2023-04-26 15:33:10 +0800

    初始化前后端

--- study-project-backend/src/main/java/ubuntu/cola/StudyProjectBackendApplication.java
+++ study-project-backend/src/main/java/ubuntu/cola/StudyProjectBackendApplication.java
@@ -1,13 +1,16 @@
 package ubuntu.cola;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class StudyProjectBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudyProjectBackendApplication.class, args);
+		log.info("StudyProjectQingKongUbuntu 项目启动成功 ");
 	}
 
 }
